031d15fb334cf9c178d7a7ecd75d633142be4786
Brody Blair <brocbla@umich.edu>
2026-02-10 18:29:28 -0600

    feat: update date maker

--- src/routes/nyt/+page.server.ts
+++ src/routes/nyt/+page.server.ts
@@ -1,5 +1,15 @@
+const getCentralDate = () => {
+	const formatter = new Intl.DateTimeFormat('en-CA', { 
+		timeZone: 'America/Chicago',
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit'
+	});
+	return formatter.format(new Date());
+};
+
 export const load = async ({ fetch, url }) => {
-	const date = url.searchParams.get('date') || new Date().toISOString().split('T')[0];
+	const date = url.searchParams.get('date') || getCentralDate();
 
 	let apiUrl = 'https://www.nytimes.com/svc/crosswords/v6/puzzle/mini.json';
 	if (date && /^\d{4}-\d{2}-\d{2}$/.test(date)) {
